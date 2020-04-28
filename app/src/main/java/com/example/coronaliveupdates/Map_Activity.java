df2b1ad922779ec53c772a9fd55009fca04c9d8e
Mahadi Hassan Munna <43148595+MahadiMunna22@users.noreply.github.com>
2020-04-29 02:25:39 +0600

    SwipeToRefresh added

--- app/src/main/java/com/example/coronaliveupdates/Map_Activity.java
+++ app/src/main/java/com/example/coronaliveupdates/Map_Activity.java
@@ -1,9 +1,15 @@
 package com.example.coronaliveupdates;
 
+import android.content.Intent;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.webkit.PermissionRequest;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
@@ -11,9 +17,11 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-public class Map_Activity extends AppCompatActivity {
+public class Map_Activity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     WebView webView;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,5 +48,42 @@ public class Map_Activity extends AppCompatActivity {
         String webContent="<iframe style=\"width:100%\"; width=\"100%\" height=\"100%\" src=\"https://coronavirus.app/map?embed=true\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>";
         webView.loadDataWithBaseURL(null,"<style>img{display: inline;height: auto;max-width: 100%;}</style>" + webContent,"text/html", "UTF-8", null);
 
+        drawerLayout = findViewById(R.id.DrawerLayout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(toggle.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.menu_home) {
+            Intent i = new Intent(getApplicationContext(), Home_Activity.class);
+            startActivity(i);
+        }
+        else if(item.getItemId() == R.id.menu_map) {
+            Intent i = new Intent(getApplicationContext(), Map_Activity.class);
+            startActivity(i);
+        }
+        else if(item.getItemId() == R.id.menu_coronatest) {
+            Intent i = new Intent(getApplicationContext(), CoronaTest.class);
+            startActivity(i);
+        }
+        else if(item.getItemId() == R.id.menu_aboutus) {
+            Intent i = new Intent(getApplicationContext(), AboutUs.class);
+            startActivity(i);
+        }
+        return false;
     }
 }
