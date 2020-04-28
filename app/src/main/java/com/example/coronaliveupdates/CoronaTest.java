98fdc65ec37ad37482b6a2c4c3dd8c9b26839522
Mahadi Hassan Munna <43148595+MahadiMunna22@users.noreply.github.com>
2020-04-29 02:43:00 +0600

    Corona Test Added

--- app/src/main/java/com/example/coronaliveupdates/CoronaTest.java
+++ app/src/main/java/com/example/coronaliveupdates/CoronaTest.java
@@ -1,13 +1,89 @@
 package com.example.coronaliveupdates;
 
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-public class CoronaTest extends AppCompatActivity {
+public class CoronaTest extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
+    WebView webView;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_corona_test);
+        setContentView(R.layout.activity_map);
+
+        webView = findViewById(R.id.MapWebView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAppCacheEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return super.shouldOverrideUrlLoading(view, request);
+            }
+        });
+        webView.setWebChromeClient(new WebChromeClient() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onPermissionRequest(PermissionRequest request) {
+                request.grant(request.getResources());
+            }
+        });
+        //String webContent = "<iframe style=\"width:100%\"; width=\"100%\" height=\"100%\" src=\"https://coronavirus.app/map?embed=true\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>";
+        webView.loadUrl("http://coronastatebd.mist.ac.bd/checker/index.php");
+
+        drawerLayout = findViewById(R.id.DrawerLayout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(toggle.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.menu_home) {
+            Intent i = new Intent(getApplicationContext(), Home_Activity.class);
+            startActivity(i);
+        }
+        else if(item.getItemId() == R.id.menu_map) {
+            Intent i = new Intent(getApplicationContext(), Map_Activity.class);
+            startActivity(i);
+        }
+        else if(item.getItemId() == R.id.menu_coronatest) {
+            Intent i = new Intent(getApplicationContext(), CoronaTest.class);
+            startActivity(i);
+        }
+        else if(item.getItemId() == R.id.menu_aboutus) {
+            Intent i = new Intent(getApplicationContext(), AboutUs.class);
+            startActivity(i);
+        }
+        return false;
     }
 }
